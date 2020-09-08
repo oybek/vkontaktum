67e88e64433fd0bb2d16f31a2e1b9d98c98266d7
oybek <hashimov@tochka.com>
2020-09-08 13:21:48 +0500

    fix

--- src/main/scala/io/github/oybek/vk4s/api/GetMessageById.scala
+++ src/main/scala/io/github/oybek/vk4s/api/GetMessageById.scala
@@ -2,9 +2,17 @@ package io.github.oybek.vk4s.api
 
 import io.github.oybek.vk4s.domain.{MessageNew, WallPostNew}
 
-case class GetMessageByIdReq(messageIds: List[Long]) extends Req {
+case class GetMessageByIdReq(messageIds: List[Long],
+                             version: String,
+                             accessToken: String) extends Req {
   def toRequestStr: String =
-    "message_ids=" + messageIds.mkString(",")
+    Seq(
+      "v" -> version,
+      "access_token" -> accessToken,
+      "message_ids" -> messageIds.mkString(",")
+    ).map {
+      case (k, v) => k + "=" + v
+    }.mkString("&")
 }
 
 case class GetMessageByIdRes(response: Response[MessageNew])
